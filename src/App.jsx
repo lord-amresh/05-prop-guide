c7c8a2b1dfb6fa50f78a062d34d047e62c77258e
lord-amresh <amresh2006@gmail.com>
2026-04-03 07:07:30 +0545

    2 - Setting up vite app for props in react

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ChildrenProps from './components/ChildrenProps.jsx'
 import Complexprops from './components/Complexprops.jsx'
 import ThemeToggler from './components/ThemeToggler.jsx'
 
-function navigation () {
+function Navigation () {
   const isdark = true
 
   const sections = [
@@ -18,12 +18,31 @@ function navigation () {
   ]
 
   return (
-    <nav className='{`sticky top-0 z-50 shadow-md`}'></nav>
+    <nav className={`sticky top-0 z-50 shadow-md
+    transition-colors ${
+      isdark ? 'bg-gray-800' : 'bg-white'
+    }`}>
+      <div className='container mx-auto px-4 py-4'>
+        <div className='flex flex-wrap gap-2 justify-center'>
+          {sections.map((section) => (
+            <button
+            className={`px-4 py-2 rounded-lg font-medium bg-blue-600 text-white mr-2 mt-2`}
+              key={section.id}
+              >
+                <span>{section.icon}</span>
+                {section.label}
+              </button>
+          ))}
+        </div>
+      </div>
+    </nav>
   )
 }
 
 function AppContent () {
-  return <div className={`min-h-screen bg-gray-800`}><h1 class="text-3xl text-white font-bold underline">
+  return <div className={`min-h-screen bg-gray-800`}>
+    <Navigation/>
+    <h1 className="text-3xl text-white font-bold underline">
     Hello world!
   </h1></div>;
 }
